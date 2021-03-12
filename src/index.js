c72437fbc7ded9fd1428ca0db1d67524c1d20410
capricornus44 <antonzavalniuk@gmail.com>
2021-03-12 09:52:57 +0100

    add condition if INPUT is empty

--- src/index.js
+++ src/index.js
@@ -9,6 +9,14 @@ const refs = getRefs();
 refs.search.addEventListener('input', debounce(onSearch, 500));
 
 function onSearch(event) {
+  /*
+  It is advisable to clear the content at each INPUT event 
+  and not make request to the API if the INPUT IS EMPTY
+  */
+  refs.content.innerHTML = '';
+  if (!event.target.value) {
+    return;
+  }
   fetchCountriesByName(event.target.value)
     .then(countries => {
       refs.content.innerHTML = '';
